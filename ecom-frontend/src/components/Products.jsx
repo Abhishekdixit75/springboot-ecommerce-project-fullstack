391479d640f473b3a34f1dd3b6cbe5297c058725
Abhishekdixit75 <abhishekdixit8005@gmail.com>
2026-03-09 19:02:12 +0530

    Add Redux integration for product management; implement product reducer and fetch products action; update Products component to use Redux state

--- ecom-frontend/src/components/Products.jsx
+++ ecom-frontend/src/components/Products.jsx
@@ -1,30 +1,44 @@
 import { MdDescription } from "react-icons/md";
 import ProductCard from "./ProductCard";
 import { FaExclamationTriangle } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchProducts } from "../store/actions";
 
 const Products = () => {
     const isLoading = false;
     const errorMessage = "";
-    const products = [{
-        productId: 652,
-        productName: "iPhone 14 Pro Max",
-        image: "https://placehold.co/600x400",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        quantity: 10,
-        price: 12345.0,
-        discount: 10.0,
-        specialPrice: 12334.0,
-    },
-    {
-        productId: 653,
-        productName: "macbook AIR m2",
-        image: "https://placehold.co/600x400",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        quantity: 0,
-        price: 12345.0,
-        discount: 10.0,
-        specialPrice: 12334.0,
-    }];
+
+    const {products} = useSelector(
+        (state) => state.products
+    );
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchProducts());
+    }, [dispatch]);
+
+    
+    // const products = [{
+    //     productId: 652,
+    //     productName: "iPhone 14 Pro Max",
+    //     image: "https://placehold.co/600x400",
+    //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    //     quantity: 10,
+    //     price: 12345.0,
+    //     discount: 10.0,
+    //     specialPrice: 12334.0,
+    // },
+    // {
+    //     productId: 653,
+    //     productName: "macbook AIR m2",
+    //     image: "https://placehold.co/600x400",
+    //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    //     quantity: 0,
+    //     price: 12345.0,
+    //     discount: 10.0,
+    //     specialPrice: 12334.0,
+    // }];
     return (
         <div className="lg:px-14 sm:px-8 px-4 py-14 2xl:w-[90%] 2xl:mx-auto">
             {isLoading ? (
