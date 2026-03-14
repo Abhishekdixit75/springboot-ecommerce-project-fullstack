a592fab61a5bde2e078b703e2c040daee74e2a4e
Abhishekdixit75 <abhishekdixit8005@gmail.com>
2026-03-14 17:56:24 +0530

    Add About component and integrate routing for About and Contact pages

--- ecom-frontend/src/App.jsx
+++ ecom-frontend/src/App.jsx
@@ -3,6 +3,8 @@ import Home from './components/home/Home';
 import Products from './components/products/Products';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/shared/Navbar';
+import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
 
@@ -12,6 +14,8 @@ function App() {
       <Routes>
         <Route path='/' element = {<Home />}></Route>
         <Route path='/products' element = {<Products />}></Route>
+        <Route path='/about' element = {<About />}></Route>
+        <Route path='/contact' element = {<Contact />}></Route>
       </Routes>
     </Router>
   )
