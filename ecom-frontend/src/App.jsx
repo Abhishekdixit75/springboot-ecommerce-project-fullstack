8ee6478bb1e7262f80330fb92224647eb0f2cd9f
Abhishekdixit75 <abhishekdixit8005@gmail.com>
2026-03-17 01:07:07 +0530

    Add Cart component and integrate cart functionality with Redux

--- ecom-frontend/src/App.jsx
+++ ecom-frontend/src/App.jsx
@@ -1,24 +1,30 @@
-import './App.css'
-import Home from './components/home/Home';
-import Products from './components/products/Products';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './components/shared/Navbar';
-import About from './components/About';
-import Contact from './components/Contact';
+import "./App.css";
+import Home from "./components/home/Home";
+import Products from "./components/products/Products";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Navbar from "./components/shared/Navbar";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import {Toaster} from "react-hot-toast";
+import React from "react";
+import Cart from "./components/cart/Cart";
 
 function App() {
-
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path='/' element = {<Home />}></Route>
-        <Route path='/products' element = {<Products />}></Route>
-        <Route path='/about' element = {<About />}></Route>
-        <Route path='/contact' element = {<Contact />}></Route>
-      </Routes>
-    </Router>
-  )
+	return (
+		<React.Fragment>
+			<Router>
+				<Navbar />
+				<Routes>
+					<Route path="/" element={<Home />}></Route>
+					<Route path="/products" element={<Products />}></Route>
+					<Route path="/about" element={<About />}></Route>
+					<Route path="/contact" element={<Contact />}></Route>
+					<Route path="/cart" element={<Cart />}></Route>
+				</Routes>
+			</Router>
+			<Toaster position="bottom-center" />
+		</React.Fragment>
+	);
 }
 
 export default App;
