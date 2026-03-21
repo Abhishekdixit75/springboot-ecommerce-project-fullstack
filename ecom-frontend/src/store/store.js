d47c153b753e2fb37c21aed7001a85122a0d126b
Abhishekdixit75 <abhishekdixit8005@gmail.com>
2026-03-21 10:13:36 +0530

    Add authReducer to manage user authentication state

--- ecom-frontend/src/store/store.js
+++ ecom-frontend/src/store/store.js
@@ -2,10 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import { productReducer } from "./reducers/productReducer";
 import errorReducer from "./reducers/errorReducer";
 import { cartReducer } from "./reducers/cartReducer";
+import { authReducer } from "./reducers/authReucer";
+
+const user = localStorage.getItem("auth") ? JSON.parse(localStorage.getItem("auth")) : [];
 
 const cartItems = localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")) : [];
 
 const initialState = {
+    auth: {user: user},
     carts : {cart: cartItems},
 };
 
@@ -13,7 +17,8 @@ export const store = configureStore({
     reducer: {
         products: productReducer,
         errors: errorReducer,
-        carts: cartReducer
+        carts: cartReducer,
+        auth: authReducer
     },
     preloadedState: initialState,
 });
