5d3fca6d8485c4da1c22b7326af6edfdc42308bb
Abhishekdixit75 <abhishekdixit8005@gmail.com>
2026-04-08 19:14:53 +0530

    feat: add OrderSummary component and integrate payment method handling in Checkout

--- ecom-frontend/src/components/checkout/Checkout.jsx
+++ ecom-frontend/src/components/checkout/Checkout.jsx
@@ -9,15 +9,19 @@ import toast from "react-hot-toast";
 import Skeleton from "../shared/CustomSkeleton";
 import ErrorPage from "../shared/ErrorPage";
 import PaymentMethod from "./PaymentMethod";
+import OrderSummary from "./OrderSummary";
+import StripePayment from "./StripePayment";
+import PaypalPayment from "./PaypalPayment";
 
 const Checkout = () => {
 	const [activeStep, setActiveStep] = useState(0);
 	const dispatch = useDispatch();
 	const {isLoading, errorMessage} = useSelector((state) => state.errors);
+	const {cart, totalPrice} = useSelector((state) => state.carts);
 	const {address, selectedUserCheckoutAddress} = useSelector(
 		(state) => state.auth,
 	);
-	const paymentMethod = false;
+	const {paymentMethod} = useSelector((state) => state.payment);
 
 	const handleBack = () => {
 		setActiveStep((prev) => prev - 1);
@@ -59,6 +63,23 @@ const Checkout = () => {
 				<div className="mt-5">
 					{activeStep === 0 && <AddressInfo address={address} />}
 					{activeStep === 1 && <PaymentMethod />}
+					{activeStep === 2 && (
+						<OrderSummary
+							totalPrice={totalPrice}
+							cart={cart}
+							address={selectedUserCheckoutAddress}
+							paymentMethod={paymentMethod}
+						/>
+					)}
+					{activeStep === 3 && 
+						<>
+						{paymentMethod === "Stripe" ? (
+							<StripePayment />
+						) : (
+							<PaypalPayment />
+						)}
+						</>
+					}
 				</div>
 			)}
 
@@ -100,7 +121,7 @@ const Checkout = () => {
 				)}
 			</div>
 
-			{errorMessage && <ErrorPage message={errorMessage}/>}
+			{errorMessage && <ErrorPage message={errorMessage} />}
 		</div>
 	);
 };
