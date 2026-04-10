daae026fc0d8eaa8f3a045a60caa61c8b4b446ab
Abhishekdixit75 <abhishekdixit8005@gmail.com>
2026-04-10 09:59:18 +0530

    style: format authReducer for consistency and readability

--- ecom-frontend/src/store/reducers/authReducer.js
+++ ecom-frontend/src/store/reducers/authReducer.js
@@ -1,26 +1,27 @@
 const initialState = {
     user: null,
     address: [],
+    clientSecret: null,
     selectedUserCheckoutAddress: null
 };
 
 export const authReducer = (state = initialState, action) => {
-    switch(action.type){
+    switch (action.type) {
         case "LOGIN_USER":
-            return {...state, user: action.payload};
-        
+            return { ...state, user: action.payload };
+
         case "LOGOUT_USER":
-            return {...state, user: null, address: null, selectedUserCheckoutAddress: null};
-        
+            return { ...state, user: null, address: null, clientSecret: null, selectedUserCheckoutAddress: null };
+
         case "USER_ADDRESS":
-            return {...state, address: action.payload};
-        
+            return { ...state, address: action.payload };
+
         case "SELECT_USER_CHECKOUT_ADDRESS":
-            return {...state, selectedUserCheckoutAddress: action.payload};
-        
+            return { ...state, selectedUserCheckoutAddress: action.payload };
+
         case "REMOVE_SELECTED_USER_CHECKOUT_ADDRESS":
-            return {...state, selectedUserCheckoutAddress: null};
-            
+            return { ...state, selectedUserCheckoutAddress: null };
+
         default:
             return state;
     }
