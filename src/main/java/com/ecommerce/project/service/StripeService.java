5dc73e20b72ba1a55d4384e265dc090ba722d808
Abhishekdixit75 <abhishekdixit8005@gmail.com>
2026-04-10 09:59:51 +0530

    feat: implement Stripe payment processing with client secret generation

--- src/main/java/com/ecommerce/project/service/StripeService.java
+++ src/main/java/com/ecommerce/project/service/StripeService.java
@@ -1,4 +1,11 @@
 package com.ecommerce.project.service;
 
-public class StripeService {
+import com.ecommerce.project.payload.StripePaymentDTO;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import org.springframework.stereotype.Service;
+
+
+public interface StripeService {
+    PaymentIntent paymentIntent(StripePaymentDTO stripePaymentDTO) throws StripeException;
 }
