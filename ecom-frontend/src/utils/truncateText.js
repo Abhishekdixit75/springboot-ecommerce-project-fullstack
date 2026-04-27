956ca7535a52ef3484e80e0846e5f80765d5dab4
Abhishekdixit75 <abhishekdixit8005@gmail.com>
2026-04-27 23:16:35 +0530

    fix: format code in truncateText utility for consistency

--- ecom-frontend/src/utils/truncateText.js
+++ ecom-frontend/src/utils/truncateText.js
@@ -1,6 +1,6 @@
 export const truncateText = (text, charLimit = 90) => {
-    if(text?.length > charLimit){
-        return text.slice(0, charLimit+3) + '...';
+    if (text?.length > charLimit) {
+        return text.slice(0, charLimit + 3) + '...';
     }
     return text;
 };
