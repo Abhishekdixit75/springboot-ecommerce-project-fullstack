535b9b92b0f23e96c899e38ab30f65a08f5b7a54
Abhishekdixit75 <abhishekdixit8005@gmail.com>
2026-04-30 16:07:32 +0530

    feat: Implement Dashboard component with analytics overview and loading/error handling

---
+++ ecom-frontend/src/components/admin/dashboard/Dashboard.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import DashboardOverview from "./DashboardOverview";
+import {FaBoxOpen, FaShoppingCart, FaDollarSign} from "react-icons/fa";
+import {useEffect} from "react";
+import {analyticsAction} from "../../../store/actions";
+import {useDispatch, useSelector} from "react-redux";
+import Loader from "../../shared/Loader";
+import ErrorPage from "../../shared/ErrorPage";
+
+const Dashboard = () => {
+	const dispatch = useDispatch();
+	const {isLoading, errorMessage} = useSelector((state) => state.errors);
+	const {
+		analytics: {productCount, totalRevenue, totalOrders},
+	} = useSelector((state) => state.admin);
+
+	useEffect(() => {
+		dispatch(analyticsAction());
+	}, [dispatch]);
+
+	if (isLoading) {
+		return <Loader />;
+	}
+
+	if (errorMessage) {
+		return <ErrorPage message={errorMessage} />;
+	}
+
+	return (
+		<div>
+			<div
+				className="flex md:flex-row mt-8 flex-col lg:justify-between 
+          border border-slate-400 rounded-lg bg-linear-to-r
+           from-green-200 to-blue-500 shadow-lg"
+			>
+				<DashboardOverview
+					title="Total Products"
+					amount={productCount}
+					Icon={FaBoxOpen}
+				/>
+
+				<DashboardOverview
+					title="Total Orders"
+					amount={totalOrders}
+					Icon={FaShoppingCart}
+				/>
+
+				<DashboardOverview
+					title="Total Revenue"
+					amount={totalRevenue}
+					Icon={FaDollarSign}
+					revenue
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default Dashboard;
