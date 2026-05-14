4edfedcfefcf588e4a74b1e3026b81b61d0d3b71
Abhishekdixit75 <abhishekdixit8005@gmail.com>
2026-05-14 18:39:21 +0530

    refactor: reformat the tests files

--- src/test/java/com/ecommerce/project/SbEcomApplicationTests.java
+++ src/test/java/com/ecommerce/project/SbEcomApplicationTests.java
@@ -3,16 +3,17 @@ package com.ecommerce.project;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = {
-    "spring.datasource.url=jdbc:h2:mem:testdb",
-    "spring.datasource.driverClassName=org.h2.Driver",
-    "spring.datasource.username=sa",
-    "spring.datasource.password=password",
-    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
-    "stripe.secret.key=dummy",
-    "frontend.url=http://localhost:3000",
-    "spring.jpa.hibernate.ddl-auto=create-drop"
-})
+@SpringBootTest(
+        properties = {
+            "spring.datasource.url=jdbc:h2:mem:testdb",
+            "spring.datasource.driverClassName=org.h2.Driver",
+            "spring.datasource.username=sa",
+            "spring.datasource.password=password",
+            "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+            "stripe.secret.key=dummy",
+            "frontend.url=http://localhost:3000",
+            "spring.jpa.hibernate.ddl-auto=create-drop"
+        })
 class SbEcomApplicationTests {
 
     @Test
